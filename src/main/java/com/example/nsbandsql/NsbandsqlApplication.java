19801c4dd847acf72e9fdac9058d6ec9a5d037e3
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-02-28 19:25:28 -0300

    Initial commit

---
+++ src/main/java/com/example/nsbandsql/NsbandsqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.nsbandsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsbandsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsbandsqlApplication.class, args);
+	}
+
+}
