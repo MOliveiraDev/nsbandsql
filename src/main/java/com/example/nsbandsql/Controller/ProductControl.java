5949e248bc8198ff9ebc7121df1518b818c3ca9f
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-02-28 23:39:36 -0300

    test

--- src/main/java/com/example/nsbandsql/Controller/ProductControl.java
+++ src/main/java/com/example/nsbandsql/Controller/ProductControl.java
@@ -1,6 +1,15 @@
 package com.example.nsbandsql.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/product")
 public class ProductControl {
 
+    @GetMapping
+    public String hello(){
+        return "Hello World";
+    }
 }
--- src/main/java/com/example/nsbandsql/Repository/ProductRepository.java
+++ src/main/java/com/example/nsbandsql/Repository/ProductRepository.java
@@ -2,6 +2,9 @@ package com.example.nsbandsql.Repository;
 
 import com.example.nsbandsql.Model.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface ProductRepository extends JpaRepository<ProductModel, Integer> {
 
