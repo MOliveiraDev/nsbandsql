e88124a4f7715c3643c2b00b8296c0f81958d219
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-02-28 21:48:56 -0300

    a

--- src/main/java/com/example/nsbandsql/Controller/ProductC.java
+++ src/main/java/com/example/nsbandsql/Controller/ProductC.java
@@ -1,28 +1,9 @@
 package com.example.nsbandsql.Controller;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Setter
-@Getter
 @RestController
-@RequestMapping("/product")
 
 public class ProductC {
-    private Integer id;
-    private String name;
-    private Long price;
-
-    public ProductC(Integer id, String name, Long price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-
-    public ProductC() {
-
-    }
 
 }
