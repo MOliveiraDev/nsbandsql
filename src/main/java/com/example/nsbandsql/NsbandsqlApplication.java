ad8b7a41652deb603da63efacb4bdd456163f9cf
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-02-28 21:54:24 -0300

    a

--- src/main/java/com/example/nsbandsql/NsbandsqlApplication.java
+++ src/main/java/com/example/nsbandsql/NsbandsqlApplication.java
@@ -8,7 +8,6 @@ public class NsbandsqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NsbandsqlApplication.class, args);
-
 	}
 
 }
--- src/main/java/com/example/nsbandsql/Repository/interfaceP.java
+++
@@ -1,9 +0,0 @@
-package com.example.nsbandsql.Repository;
-import com.example.nsbandsql.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface interfaceP extends JpaRepository<Product, Integer> {
-
-
-}
